e5a3a8211d84588d2649f3d1e46f0bbdb310718a
Jan Fuka <jan.fuka@student.spsmb.cz>
2025-09-04 15:23:46 +0200

    hi

--- src/4rocnik/setup/setup/GuessNumber.cs
+++ src/4rocnik/setup/setup/GuessNumber.cs
@@ -19,16 +19,33 @@ namespace setup
         {
             bool guess = false;
             int numberOfGuesses = 0;
+            int playersGuess;
             int generatedNumber = generateRandomNumber();
-            while (guess == false)
+            do
             {
-                Console.WriteLine("guess the number between" + number1 + " and " + number2);
-                Console.ReadLine();
-            }
+                
+                Console.WriteLine("please enter the number you think is right");
+                playersGuess = int.Parse(Console.ReadLine());
+                if (playersGuess == generatedNumber)
+                {
+                    Console.WriteLine("nice you have guessed the number");
+                    guess = true;
+                    break;
+                }
+                else
+                {
+                    if (playersGuess > generatedNumber)
+                    {
+                        Console.WriteLine("too high");
+                    } else if (playersGuess < generatedNumber)
+                    {
+                        Console.WriteLine("too low");
+                    }
+                }
+
+
+
+            } while (guess == false);
         }
-         
-         
-        
-        
     }
 }
---
+++ src/4rocnik/setup/setup/calculator.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Globalization;
+
+namespace setup
+{
+    public class calculator
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Zadejte výpočet (např. 4 + 5):");
+            string input = Console.ReadLine();
+
+            // Rozdělení vstupu podle mezer a odstranění prázdných částí
+            string[] parts = input.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            if (parts.Length != 3)
+            {
+                Console.WriteLine("Chybný formát! Použijte: číslo operátor číslo");
+                return;
+            }
+
+            try
+            {
+                // Převod na čísla s tečkou (např. 4.5)
+                double a = double.Parse(parts[0], CultureInfo.InvariantCulture);
+                string op = parts[1];
+                double b = double.Parse(parts[2], CultureInfo.InvariantCulture);
+
+                double result = 0;
+
+                switch (op)
+                {
+                    case "+":
+                        result = a + b;
+                        break;
+                    case "-":
+                        result = a - b;
+                        break;
+                    case "*":
+                        result = a * b;
+                        break;
+                    case "/":
+                        if (b == 0)
+                        {
+                            Console.WriteLine("Chyba: Dělení nulou!");
+                            return;
+                        }
+                        result = a / b;
+                        break;
+                    case "**":
+                        result = Math.Pow(a, b);
+                        break;
+                    default:
+                        Console.WriteLine("Neplatný operátor!");
+                        return;
+                }
+
+                Console.WriteLine($"{a} {op} {b} = {result}");
+            }
+            catch
+            {
+                Console.WriteLine("Chyba při zpracování vstupu. Používejte tečku pro desetinná čísla.");
+            }
+        }
+    }
+}
