96b919dd302c6ef56de1fe59431db5540806b7f0
Jan Fuka <jan.fuka@student.spsmb.cz>
2025-09-11 15:24:40 +0200

    message

---
+++ src/4rocnik/Maturita/Files/Movie.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Files
+{
+    public class Movie
+    {
+        public string film;
+        public string genre;
+        public string leadStudio;
+        public int audienceScore;
+        public int profitability;
+        public int rottenTomatoes;
+        public int worldwideGross;
+        public int year;
+
+        
+
+        
+        public Movie(string line)
+        {
+        
+            var values = line.Split(',');
+
+           
+
+           
+            film = values[0];
+            genre = values[1];
+            leadStudio = values[2];
+
+
+            audienceScore = int.Parse(values[3]);
+            profitability = int.Parse(values[4]);
+            rottenTomatoes = int.Parse(values[5]);
+            worldwideGross = int.Parse(values[6]);
+            year = int.Parse(values[7]);
+        }
+
+       
+        public override string ToString()
+        {
+            return $"{film} ({year}) + Genre: {genre}, Studio: {leadStudio}, " +
+                   $"Audience Score: {audienceScore}, Rotten Tomatoes: {rottenTomatoes}%, " +
+                   $"Profitability: {profitability}, Worldwide Gross: {worldwideGross}";
+        }
+    }
+}
--- src/4rocnik/Maturita/Files/Program.cs
+++ src/4rocnik/Maturita/Files/Program.cs
@@ -1,9 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
 namespace Files
 {
   internal class Program
   {
     public static void Main(string[] args)
     {
+      var streamReader = new StreamReader(@"C:\Users\jan.fuka\Desktop\csharp\src\4rocnik\Maturita\Files\movies.csv");
+      Movie movie = new Movie(streamReader.ReadLine());
+      while (!streamReader.EndOfStream)
+      {
+       Console.WriteLine(streamReader.ReadToEnd().Split());
+        
+      }
+
+
+
+
     }
   }
 }
